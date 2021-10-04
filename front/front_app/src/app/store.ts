5b69e124dfc589aa024ee4ae4b55f9cd511fae45
meishimomura <mei.shimomura@icloud.com>
2021-10-05 05:50:15 +0900

    studentAuth reducerの登録

--- front/front_app/src/app/store.ts
+++ front/front_app/src/app/store.ts
@@ -1,8 +1,9 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import studentAuthReducer from "../features/student/auth/authSlice";
 export const store = configureStore({
   reducer: {
+    studentAuth: studentAuthReducer,
     counter: counterReducer,
   },
 });
@@ -15,3 +16,5 @@ export type AppThunk<ReturnType = void> = ThunkAction<
   unknown,
   Action<string>
 >;
+
+export type AppDispatch = typeof store.dispatch;
