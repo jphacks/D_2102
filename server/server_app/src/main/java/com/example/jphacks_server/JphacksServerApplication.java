49a80eac0bbe48499781605cfb5df6c76fef6061
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2021-10-06 05:56:34 +0900

    sserverのdocker-composeにmysql追加とsqlができたところまで

--- server/server_app/src/main/java/com/example/jphacks_server/JphacksServerApplication.java
+++ server/server_app/src/main/java/com/example/jphacks_server/JphacksServerApplication.java
@@ -1,17 +1,26 @@
 package com.example.jphacks_server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class JphacksServerApplication {
 
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
 	@RequestMapping("/")
 	public String home() {
-		return "Stk World";
+		List<Map<String, Object>> schools = jdbcTemplate.queryForList("select * from schools");
+		return schools.get(0).toString();
 	}
 
 	public static void main(String[] args) {
