d7c5bcd5aef48c39bee7b2a9254e5463484c0710
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2021-10-11 05:59:31 +0900

    ログイン処理仮完成

--- server/server_app/src/main/java/com/example/jphacks_server/controller/Controller.java
+++ server/server_app/src/main/java/com/example/jphacks_server/controller/Controller.java
@@ -5,7 +5,10 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.example.jphacks_server.entity.Users;
 import com.example.jphacks_server.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +28,7 @@ public class Controller {
 
 
 
-    private static final Long EXPIRATION_TIME = 1000L * 60L * 10L;
 
-    String secretKey = "secret";
-    Date issuedAt = new Date();
-    Date notBefore = new Date(issuedAt.getTime());
-    Date expiresAt = new Date(issuedAt.getTime() + EXPIRATION_TIME);
 
 
     @RequestMapping("/")
@@ -40,34 +38,22 @@ public class Controller {
 
 
 
-    @RequestMapping("/auth")
-    public String home() {
-        List<Map<String, Object>> schools = jdbcTemplate.queryForList("select * from schools");
+    @RequestMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Users usersData) {
+        String usersId = usersService.login(usersData.getUsersLoginId(), usersData.getUsersLoginPassword());
+        String token = usersService.createToken(usersId);
 
-        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        if(token == null) return ResponseEntity.badRequest().body("Token生成できない");
 
-        try {
+        HttpHeaders responseHeaders = new HttpHeaders();
 
-            String token = JWT.create()
-                    // registered claims
-                    //.withJWTId("jwtId")        //"jti" : JWT ID
-                    //.withAudience("audience")  //"aud" : Audience
-                    //.withIssuer("issuer")      //"iss" : Issuer
-                    .withSubject("test_password")         //"sub" : Subject
-                    .withIssuedAt(issuedAt)      //"iat" : Issued At
-                    .withNotBefore(notBefore)    //"nbf" : Not Before
-                    .withExpiresAt(expiresAt)    //"exp" : Expiration Time
-                    //private claims
-                    .withClaim("X-AUTHORITIES", "aaa")
-                    .withClaim("X-USERNAME", "bbb")
-                    .sign(algorithm);
-            System.out.println("generate token : " + token);
-            return token;
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        responseHeaders.set("Authorization",
+                usersService.createToken(usersId));
+
+        return ResponseEntity.ok()
+                .headers(responseHeaders)
+                .body("Response with header using ResponseEntity");
 
 
-        return schools.get(0).toString();
     }
 }
--- server/server_app/src/main/java/com/example/jphacks_server/service/UsersService.java
+++ server/server_app/src/main/java/com/example/jphacks_server/service/UsersService.java
@@ -1,5 +1,7 @@
 package com.example.jphacks_server.service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.example.jphacks_server.entity.Schools;
 import com.example.jphacks_server.entity.Users;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,10 +18,57 @@ public class UsersService {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private static final Long EXPIRATION_TIME = 1000L * 60L * 10L;
+
     public List<Users> findAll() {
         // 実行する SQL を組み立てて実行
         String query = "SELECT * from users";
         List<Users> users = jdbcTemplate.query(query,new BeanPropertyRowMapper<>(Users.class));
         return users;
+    }
+
+    public String login(String id, String password){
+        String query = "SELECT * from users where users_login_id = ? and users_login_password = ?";
+        List<Users> users = jdbcTemplate.query(query,new BeanPropertyRowMapper<>(Users.class), id, password);
+        String usersId = String.valueOf(users.get(0).getUsersId());
+        return usersId;
+
+    }
+
+
+    public String createToken(String subject) {
+
+
+        String secretKey = "panda";
+        Date issuedAt = new Date();
+        Date notBefore = new Date(issuedAt.getTime());
+        Date expiresAt = new Date(issuedAt.getTime() + EXPIRATION_TIME);
+
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+        try {
+
+            String token = JWT.create()
+                    // registered claims
+                    //.withJWTId("jwtId")        //"jti" : JWT ID
+                    //.withAudience("audience")  //"aud" : Audience
+                    //.withIssuer("issuer")      //"iss" : Issuer
+                    .withSubject(subject)         //"sub" : Subject
+                    .withIssuedAt(issuedAt)      //"iat" : Issued At
+                    .withNotBefore(notBefore)    //"nbf" : Not Before
+                    .withExpiresAt(expiresAt)    //"exp" : Expiration Time
+                    //private claims
+                    //private claims
+//                    .withClaim("X-AUTHORITIES", "aaa")
+//                    .withClaim("X-USERNAME", "bbb")
+                    .sign(algorithm);
+            System.out.println("generate token : " + token);
+            return token;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
