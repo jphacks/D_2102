e4c66f14a778ad6ad6f48b512aa0bafce80d0d23
meishimomura <mei.shimomura@icloud.com>
2021-10-11 12:48:28 +0900

    サイドバー追加

--- front/front_app/src/App.tsx
+++ front/front_app/src/App.tsx
@@ -1,13 +1,227 @@
 import React from "react";
-import Default from "./Default";
-import { Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Default} />
-    </Switch>
-  );
+import { NavLink } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./features/home/Home";
+
+import styles from "./App.module.css";
+import grey from "@material-ui/core/colors/grey";
+import { useTheme } from "@material-ui/core/styles";
+import { styled, Theme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HelpIcon from "@mui/icons-material/Help";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import SettingsRoundedIcon from "@mui/icons-material/SettingsRounded";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import SensorDoorRoundedIcon from "@mui/icons-material/SensorDoorRounded";
+import NotificationsRoundedIcon from "@mui/icons-material/NotificationsRounded";
+
+const drawerWidth = 240;
+
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })<{
+  open?: boolean;
+}>(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(3),
+  transition: theme.transitions.create("margin", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginLeft: `-${drawerWidth}px`,
+  ...(open && {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  }),
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
 }
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
+export const App: React.FC = () => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const Logout = () => {
+    localStorage.removeItem("localJWT");
+    window.location.href = "/login";
+  };
+
+  const current = {
+    color: "#0679EB",
+    textDecoration: "none",
+    background: grey[100],
+    width: "100%",
+    display: "inline-block",
+  };
+
+  return (
+    <div>
+      <Box sx={{ display: "flex" }}>
+        <AppBar
+          position="fixed"
+          open={open}
+          style={{ backgroundColor: "#0679EB" }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{ mr: 2, ...(open && { display: "none" }) }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              ロゴが入ります
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="persistent"
+          anchor="left"
+          open={open}
+        >
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === "ltr" ? (
+                <ChevronLeftIcon />
+              ) : (
+                <ChevronRightIcon />
+              )}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            <NavLink exact to="/home" activeStyle={current}>
+              <ListItem button>
+                <ListItemIcon>
+                  <HomeRoundedIcon />
+                </ListItemIcon>
+                <ListItemText primary="ホーム" />
+              </ListItem>
+            </NavLink>
+            <NavLink
+              exact
+              to="/"
+              className={styles.default__nav}
+              activeStyle={current}
+            >
+              <ListItem button>
+                <ListItemIcon>
+                  <NotificationsRoundedIcon />
+                </ListItemIcon>
+                <ListItemText primary="通知" />
+              </ListItem>
+            </NavLink>
+            {["1年数学", "1年国語", "1年理科", "1年社会"].map((text, index) => (
+              <NavLink
+                exact
+                to="/"
+                className={styles.default__nav}
+                activeStyle={current}
+                key={index}
+              >
+                <ListItem button key={text}>
+                  <ListItemIcon>
+                    <SensorDoorRoundedIcon />
+                  </ListItemIcon>
+                  <ListItemText primary={text + "の部屋"} />
+                </ListItem>
+              </NavLink>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            <ListItem button onClick={Logout}>
+              <ListItemIcon>
+                <SettingsRoundedIcon />
+              </ListItemIcon>
+              <ListItemText primary="設定" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon>
+                <HelpIcon />
+              </ListItemIcon>
+              <ListItemText primary="使い方" />
+            </ListItem>
+            <ListItem button onClick={Logout}>
+              <ListItemIcon>
+                <ExitToAppIcon />
+              </ListItemIcon>
+              <ListItemText primary="ログアウト" />
+            </ListItem>
+          </List>
+        </Drawer>
+        <Main open={open}>
+          <DrawerHeader />
+          <Switch>
+            <Route exact path="/home" component={Home} />
+          </Switch>
+        </Main>
+      </Box>
+    </div>
+  );
+};
 
 export default App;
