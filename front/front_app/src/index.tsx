fa75bdcb949472089a2ae1f5f47f57e49a714b50
meishimomura <mei.shimomura@icloud.com>
2021-10-11 12:50:14 +0900

    統一テーマ追加

--- front/front_app/src/index.tsx
+++ front/front_app/src/index.tsx
@@ -6,6 +6,7 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { Route, BrowserRouter } from "react-router-dom";
+
 import Auth from "./features/auth/Auth";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { createMuiTheme } from "@material-ui/core/styles";
