ccf346bddf7b0a09d73dc123bd63aa7be147fe1b
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2021-10-16 18:01:47 +0900

    テーブル一部追加・修正

--- server/server_app/src/main/java/com/example/jphacks_server/service/UsersService.java
+++ server/server_app/src/main/java/com/example/jphacks_server/service/UsersService.java
@@ -60,8 +60,8 @@ public class UsersService {
                     .withExpiresAt(expiresAt)    //"exp" : Expiration Time
                     //private claims
                     //private claims
-//                    .withClaim("X-AUTHORITIES", "aaa")
-//                    .withClaim("X-USERNAME", "bbb")
+                    //.withClaim("X-AUTHORITIES", "aaa")
+                    //.withClaim("X-USERNAME", "bbb")
                     .sign(algorithm);
             System.out.println("generate token : " + token);
             return token;
