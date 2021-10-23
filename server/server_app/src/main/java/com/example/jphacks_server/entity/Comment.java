7ad6e1662fb7bc8b9c9067585cba3a6ea14b680b
KazukiKimura-Developer <momx4639@yahoo.co.jp>
2021-10-23 22:07:37 +0900

    commentの情報を返すAPI

---
+++ server/server_app/src/main/java/com/example/jphacks_server/entity/Comment.java
@@ -0,0 +1,49 @@
+package com.example.jphacks_server.entity;
+
+public class Comment {
+    private int commentId;
+    private String subjectsName;
+    private String comment_content;
+    private String isAnswered;
+    private String createdAt;
+
+    public int getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(int commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getSubjectsName() {
+        return subjectsName;
+    }
+
+    public void setSubjectsName(String subjectsName) {
+        this.subjectsName = subjectsName;
+    }
+
+    public String getComment_content() {
+        return comment_content;
+    }
+
+    public void setComment_content(String comment_content) {
+        this.comment_content = comment_content;
+    }
+
+    public String getIsAnswered() {
+        return isAnswered;
+    }
+
+    public void setIsAnswered(String isAnswered) {
+        this.isAnswered = isAnswered;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ server/server_app/src/main/java/com/example/jphacks_server/service/CommentService.java
@@ -0,0 +1,48 @@
+package com.example.jphacks_server.service;
+
+import com.example.jphacks_server.entity.Comment;
+import com.example.jphacks_server.entity.Subject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public ResponseEntity<String> comentAll(String id, HttpHeaders header){
+        String query = "select comments.comment_id, subjects.subjects_name, comments.comment_content, comments.created_at, if(comments.comment_is_answered = 0, \"notAnswered\",\"Answered\") as is_answered\n" +
+                "from comments\n" +
+                "left join subjects on comments.subjects_id= subjects.subjects_id \n" +
+                "left join users on  comments.users_id= users.users_id\n" +
+                "left join student_group on users.student_group_id = student_group.student_group_id\n" +
+                "where student_group.student_group_id = (select student_group_id from users where users_id = ?)\n" +
+                "and subjects.subject_one_to_one = 0\n" +
+                "order by comments.created_at desc";
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode root = mapper.createObjectNode();
+        List<Comment> users = jdbcTemplate.query(query,new BeanPropertyRowMapper<>(Comment.class), Integer.parseInt(id));
+
+        ResponseEntity<String> responseEntity = null;
+        try {
+            responseEntity = new ResponseEntity<String>(mapper.writeValueAsString(users), header, HttpStatus.OK);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return responseEntity;
+
+    }
+}
