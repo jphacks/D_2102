6f56ef8028eab2c7077243445514d0356a99f174
下村芽生 <mei.shimomura.hal@gmail.com>
2021-10-24 17:37:29 +0900

    storeを追加

--- front/front_app/src/app/store.ts
+++ front/front_app/src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
+import studentHomeReducer from "../features/home/studentHomeSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    studentHome: studentHomeReducer,
     counter: counterReducer,
   },
 });
