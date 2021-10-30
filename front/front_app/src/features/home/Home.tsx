5396065cbde183a5753e9615b51f20e97f80afd3
下村芽生 <mei.shimomura.hal@gmail.com>
2021-10-30 09:30:31 +0900

    Homeコンポーネントを教師と生徒で切り替える

--- front/front_app/src/features/home/Home.tsx
+++ front/front_app/src/features/home/Home.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 import App from "../../App";
 import StudentHome from "./StudentHome";
+import TeacherHome from "./TeacherHome";
 
 const Home: React.FC = () => {
+  const userType = localStorage.getItem("localUserTyoe");
   return (
-    <App>
-      <StudentHome />
-    </App>
+    <App>{userType === "student" ? <StudentHome /> : <TeacherHome />}</App>
   );
 };
 
