bd77998da0e025293b6a361f78e787a12a07889d
下村芽生 <mei.shimomura.hal@gmail.com>
2021-10-30 10:28:30 +0900

    教師サイドHomeコンポーネント作成

--- front/front_app/src/features/home/TeacherHome.tsx
+++ front/front_app/src/features/home/TeacherHome.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { AppDispatch } from "../../app/store";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectComments,
+  fetchAsyncGetNotAnsweredComment,
+} from "./teacherHomeSlice";
+
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import styles from "./TeacherHome.module.css";
+import { Link } from "react-router-dom";
+import Paper from "@material-ui/core/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+
+const useStyles = makeStyles((theme: Theme) => ({}));
 
 const TeacherHome: React.FC = () => {
-  return <div></div>;
+  const classes = useStyles();
+  const dispatch: AppDispatch = useDispatch();
+
+  const comments = useSelector(selectComments);
+
+  useEffect(() => {
+    const fetchBootLoader = async () => {
+      await dispatch(fetchAsyncGetNotAnsweredComment());
+    };
+    fetchBootLoader();
+  }, [dispatch]);
+
+  return (
+    <div>
+      <h2 className={styles.teacher_home__h2}>未回答の質問</h2>
+      <Paper>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              {comments.map((comment, i) => (
+                <TableRow key={i} hover>
+                  <Link
+                    to={"/comment/" + comment.commentId}
+                    className={styles.teacher_home__nav}
+                  >
+                    <TableCell>{comment.vote}知りたい！</TableCell>
+                    <TableCell>
+                      {comment.isAnswered === "Answered"
+                        ? "回答済み"
+                        : "未回答"}
+                    </TableCell>
+                    <TableCell>{comment.createdAt}</TableCell>
+                    <TableCell>{comment.subjectsName}</TableCell>
+                    <TableCell>{comment.commentContent}</TableCell>
+                  </Link>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+    </div>
+  );
 };
 
 export default TeacherHome;
